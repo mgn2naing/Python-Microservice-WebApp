c0940c2cfee432b79666b937a65e2094a9e74d24
Nyi Nyi Naing <mgnyinaing@outlook.com>
2021-10-13 17:30:33 +0630

    add views, serializer and urls

--- products/views.py
+++ products/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product
+from .serializer import ProductSerializer
+from rest_framework.response import Response
+class ProductViewSet(viewsets.ViewSet):
+
+    def list(self, request): #/api/product/
+        product = Product.objects.all()
+        serializer = ProductSerializer(product, Many=True)
+        return Response(serializer.data)
+
+
+    def create(self, request): #/api/product/
+        pass
+
+    def retrive(self, request, pk=None): #/api/product/<str:id>
+        pass
+
+    def update(self, request, pk=None): #/api/product/<str:id>
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
+
 
 # Create your views here.
